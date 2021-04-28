4f63abefe20f1160d05b58ce97e95445d92661e3
Vansh <vvanshg@gmail.com>
2021-04-28 15:28:29 +0530

    MongoDB connected and Routes Defined

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+//connect to database
+connectDB();
+
+//Define Routes
+
+app.use("/api/users" , require("./routes/api/users")) ;
+app.use("/api/posts" , require("./routes/api/posts")) ;
+app.use("/api/auth" , require("./routes/api/auth")) ;
+app.use("/api/profile" , require("./routes/api/profile")) ;
+
+
+
+
 
 app.get("/" , (req , res) => {
     res.send("API Running");
 })
+
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT , () => {
     console.log(`Server started successfuly on ${PORT}`);
