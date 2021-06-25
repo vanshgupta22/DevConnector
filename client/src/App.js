0193b1384246311729afe7c0eac81e329f38baac
Vansh <vvanshg@gmail.com>
2021-06-25 13:29:48 +0530

    feat : Add Private Route Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 //Redux
 import {Provider} from 'react-redux';
 import store from './store';
@@ -32,6 +34,7 @@ return (
     <Switch>
       <Route exact path = '/login' component = {Login}/>
       <Route exact path = '/register' component = {Register}/>
+      <PrivateRoute exact path = '/dashboard' component = {Dashboard}/>
     </Switch>
 
     </section>
