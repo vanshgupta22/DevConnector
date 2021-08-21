e573a03200049f7c5faa495005aa4a6e00a0e442
Vansh <vvanshg@gmail.com>
2021-08-22 02:03:47 +0530

    feat : Added Experience and Education React

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { setAlert } from "./alert";
 import {
     GET_PROFILE,
-    PROFILE_ERROR
+    PROFILE_ERROR,
+    UPDATE_PROFILE
 } from "./types";
 
 //GET current user profile
@@ -69,4 +70,83 @@ export const createProfile = (formData , history , edit = false) => async dispat
     }
 }
 
+//Add Experience
+
+export const addExperience = (formData , history) => async dispatch => {
+    try {
+
+        const config = {
+            headers : {
+                'Content-Type' : 'application/json'
+            }
+        }
+
+        const res = await axios.put('/api/profile/experience' , formData , config);
+
+        dispatch({
+            type : UPDATE_PROFILE,
+            payload : res.data
+        })
+
+        dispatch(setAlert('Experience Added' , 'success'));
+
+            history.push('/dashboard')
+        
+        
+    } catch (err) {
+
+        const errors = err.response.data.errors; // as we get an errors array in backend
+        if(errors){
+            errors.forEach(error => {
+                dispatch(setAlert(error.msg , 'danger'));
+                
+            });
+        }
+        
+        dispatch({
+            type : PROFILE_ERROR,
+            payload : {msg : err.response.statusText , status : err.response.status}
+        })
+    }
+}
+
+//Add Education
+
+export const addEducation = (formData , history) => async dispatch => {
+    try {
+
+        const config = {
+            headers : {
+                'Content-Type' : 'application/json'
+            }
+        }
+
+        const res = await axios.put('/api/profile/education' , formData , config);
+
+        dispatch({
+            type : UPDATE_PROFILE,
+            payload : res.data
+        })
+
+        dispatch(setAlert('Education Added' , 'success'));
+
+            history.push('/dashboard')
+        
+        
+    } catch (err) {
+
+        const errors = err.response.data.errors; // as we get an errors array in backend
+        if(errors){
+            errors.forEach(error => {
+                dispatch(setAlert(error.msg , 'danger'));
+                
+            });
+        }
+        
+        dispatch({
+            type : PROFILE_ERROR,
+            payload : {msg : err.response.statusText , status : err.response.status}
+        })
+    }
+}
 
