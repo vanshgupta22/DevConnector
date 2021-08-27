23df2e9e3c6807e7ef8b5d5f41fffc20070a9997
Vansh <vvanshg@gmail.com>
2021-08-27 11:04:44 +0530

    feat : Populated Individual profile with experience and education

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,10 @@ import Spinner from '../layout/Spinner';
 import {getProfileById} from '../../actions/profile'
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
+
+
 
 
 const Profile = ({getProfileById , match , profile : {profile , loading} , auth}) => {
@@ -28,6 +32,27 @@ const Profile = ({getProfileById , match , profile : {profile , loading} , auth}
                <div class="profile-grid my-1">
                    <ProfileTop profile = {profile} />
                    <ProfileAbout profile = {profile} />
+                   <div className="profile-exp bg-white p-2">
+                       <h2 className="text-primary">Experience</h2>
+                       {profile.experience.length > 0 ? (<Fragment>
+                           {profile.experience.map(experience => (
+                              <ProfileExperience key = {experience._id} experience = {experience} /> 
+                           ))}
+                       </Fragment>) : (<h4>No Experience Credentials</h4>)}
+
+                   </div>
+
+                   <div className="profile-edu bg-white p-2">
+                       <h2 className="text-primary">Education</h2>
+                       {profile.education.length > 0 ? (<Fragment>
+                           {profile.education.map(education => (
+                              <ProfileEducation key = {education._id} education = {education} /> 
+                           ))}
+                       </Fragment>) : (<h4>No Education Credentials</h4>)}
+
+                   </div>
+
+                   
 
                </div>
            </Fragment>}
