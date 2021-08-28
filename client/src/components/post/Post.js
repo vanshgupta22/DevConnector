b0a16459e4955e5eafa4c9b1fbe4896ce5852242
Vansh <vvanshg@gmail.com>
2021-08-28 10:21:00 +0530

    feat : add comment frontend

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import {connect} from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
 import {getPost} from '../../actions/post'
+import CommentForm from './CommentForm'
 
 const Post = ({getPost , post : {post , loading} , match }) => {
 
@@ -15,6 +16,7 @@ const Post = ({getPost , post : {post , loading} , match }) => {
     return loading || post === null ? <Spinner /> : <Fragment>
     <Link to = '/posts' className="btn">Back to Posts</Link>
     <PostItem post = {post} showActions = {false} />
+    <CommentForm postId = {post._id} />
     </Fragment> 
     
 }
