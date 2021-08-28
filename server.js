af485662a9c1355b701a21b7261e87599050d6f4
Vansh <vvanshg@gmail.com>
2021-08-28 13:54:02 +0530

    Prepared for Deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path")
 
 const app = express();
 
@@ -16,13 +17,18 @@ app.use("/api/posts" , require("./routes/api/posts")) ;
 app.use("/api/auth" , require("./routes/api/auth")) ;
 app.use("/api/profile" , require("./routes/api/profile")) ;
 
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    // set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*' , (req , res) => {
+        res.sendFile(path.resolve(__dirname, 'client' , 'build' , 'index.html'))
+    })
+}
 
 
 
-
-app.get("/" , (req , res) => {
-    res.send("API Running");
-})
 
 
 const PORT = process.env.PORT || 5000;
