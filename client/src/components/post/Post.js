8ffcd20f7b614e3195b56be7abaedd6ffa603808
Vansh <vvanshg@gmail.com>
2021-08-28 10:54:05 +0530

    feat : delete comment added and post route completed frontend

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,6 +6,8 @@ import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
 import {getPost} from '../../actions/post'
 import CommentForm from './CommentForm'
+import CommentItem from './CommentItem'
+
 
 const Post = ({getPost , post : {post , loading} , match }) => {
 
@@ -17,6 +19,11 @@ const Post = ({getPost , post : {post , loading} , match }) => {
     <Link to = '/posts' className="btn">Back to Posts</Link>
     <PostItem post = {post} showActions = {false} />
     <CommentForm postId = {post._id} />
+    <div className="comments">
+        {post.comments.map(comment => (
+            <CommentItem key = {comment._id} comment = {comment} postId = {post._id} />
+        ))}
+    </div>
     </Fragment> 
     
 }
